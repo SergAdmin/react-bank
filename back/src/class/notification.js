b11489c347048cc624e61e89aae677966f0ae72c
Sergii <52467090+SergAdmin@users.noreply.github.com>
2023-11-28 01:11:45 +0200

    react-bank v.01

---
+++ back/src/class/notification.js
@@ -0,0 +1,18 @@
+//визначення типу операції
+
+class Notification {
+  constructor(type, text) {
+    this.type = this.isValidType(type) ? type : 'Unknown'
+    this.text = text
+    this.date = new Date()
+    this.id = new Date().getTime()
+  }
+
+  isValidType(type) {
+    return type === 'ANNOUNCE' || type === 'Warning'
+  }
+}
+
+module.exports = {
+  Notification,
+}
